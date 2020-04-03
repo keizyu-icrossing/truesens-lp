171086f74d20443319fe0cb8974c851038674246
Keizyu Delgadillo <keizyu@me.com>
2020-04-03 10:58:32 -0600

    Se maquetan componentes

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -11,8 +11,28 @@
   'use strict';
 
   $(function () {
-    
-    var mySwiper = new Swiper ('.swiper-container', {
+
+    new Swiper ('.hero-slider .swiper-container', {
+      loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      }
+    });
+
+    new Swiper('.product-slider .swiper-container', {
+      slidesPerView: 4,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
+
+    new Swiper ('.blockquote-slider .swiper-container', {
       loop: true,
       pagination: {
         el: '.swiper-pagination',
