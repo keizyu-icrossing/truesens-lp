91b81455526365b7809a9f6948f0d61d7277baa3
Keizyu Delgadillo <keizyu@me.com>
2020-04-25 17:34:58 -0500

    UPDATES, bug fixes, responsive, content info updated.

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -4,36 +4,54 @@
 
   $(function () {
 
-    new Swiper ('.hero-slider .swiper-container', {
+    var galleryHero = new Swiper('.hero-slider .swiper-container', {
       loop: true,
       pagination: {
         el: '.swiper-pagination',
       },
       navigation: {
         nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        prevEl: '.swiper-button-prev'
       }
     });
 
-    new Swiper('.product-slider .swiper-container', {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+      spaceBetween: 10,
+      slidesPerView: 5,
+      // loop: true,
+      // freeMode: true,
+      // loopedSlides: 5, //looped slides should be the same
+      watchSlidesVisibility: true,
+      watchSlidesProgress: true,
     });
 
-    new Swiper ('.blockquote-slider .swiper-container', {
+    var galleryTop = new Swiper('.gallery-top', {
+      // spaceBetween: 10,
+      // loop:true,
+      // loopedSlides: 5, //looped slides should be the same
+      thumbs: {
+        swiper: galleryThumbs,
+      },
+      // autoplay: true,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      },
+      effect: 'fade',
+      fadeEffect: {
+          crossFade: true
+      }
+    });
+
+
+    var galleryTestimonials = new Swiper ('.blockquote-slider .swiper-container', {
       loop: true,
       pagination: {
         el: '.swiper-pagination',
+        clickable: true
       },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      }
     });
+
 
   });
 
